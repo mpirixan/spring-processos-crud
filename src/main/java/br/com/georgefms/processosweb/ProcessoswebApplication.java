04e87fedc456e5b5cf6ee9adca0a448258e4ad32
George Felipe de Magalhães Silva <georgefelipe8@gmail.com>
2022-11-06 14:33:47 -0300

    Initial Commit

---
+++ src/main/java/br/com/georgefms/processosweb/ProcessoswebApplication.java
@@ -0,0 +1,13 @@
+package br.com.georgefms.processosweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessoswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessoswebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/georgefms/processosweb/ProcessoswebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.georgefms.processosweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessoswebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
