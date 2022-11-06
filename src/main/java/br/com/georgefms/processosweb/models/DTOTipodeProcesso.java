b8b980b06353e66e0fb38f28ec168567ab8913a9
George Felipe de Magalhães Silva <georgefelipe8@gmail.com>
2022-11-06 16:59:57 -0300

    Feat: Create e intermediate DTO classs

---
+++ src/main/java/br/com/georgefms/processosweb/models/DTOTipodeProcesso.java
@@ -0,0 +1,33 @@
+package br.com.georgefms.processosweb.models;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+//Clase responsável por evitar envios indesejados pelo lado do servidor
+//DTO -> DATA TRANSFERING OBJECT
+public class DTOTipodeProcesso {
+    @NotNull
+    @NotBlank
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public TipodeProcesso toTipoProcesso(){
+        TipodeProcesso tipodeProcesso = new TipodeProcesso();
+        tipodeProcesso.setNome(this.nome);
+        return tipodeProcesso;
+    }
+
+    @Override
+    public String toString() {
+        return "DTOTipodeProcesso{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
+}
