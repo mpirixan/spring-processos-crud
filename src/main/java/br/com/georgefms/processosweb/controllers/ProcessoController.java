953c0dd181edae0dab18451c35cd5b7057c68fa0
George Felipe de Magalhães Silva <georgefelipe8@gmail.com>
2022-11-07 15:58:33 -0300

    Feat: Implementing show, edit , delete and update

--- src/main/java/br/com/georgefms/processosweb/controllers/ProcessoController.java
+++ src/main/java/br/com/georgefms/processosweb/controllers/ProcessoController.java
@@ -4,20 +4,15 @@ import br.com.georgefms.processosweb.models.DtoProcesso;
 import br.com.georgefms.processosweb.models.Processo;
 import br.com.georgefms.processosweb.repositories.ProcessoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/processos")
@@ -51,6 +46,69 @@ public class ProcessoController {
         Processo processo = requisicao.toProcesso();
         this.repository.save(processo);
         return new ModelAndView("redirect:/processos");
+    }
+
+    //Mostrar um registro
+    @GetMapping("/{id}")
+    public ModelAndView show(@PathVariable Long id){
+        Optional<Processo> opcional = this.repository.findById(id);
+        if(opcional.isPresent()){
+            Processo processo = opcional.get();
+            ModelAndView mv = new ModelAndView("processos/show");
+            mv.addObject("processo", processo);
+            return mv;
+        }
+        return new ModelAndView("redirect:/processoss");
+    }
+
+    //Editar o Registro
+    @GetMapping("/{id}/edit" )
+    public ModelAndView edit(@PathVariable long id, DtoProcesso requisicao){
+        Optional<Processo> opcional = this.repository.findById(id);
+        if (opcional.isPresent()){
+            Processo processo = opcional.get();
+            requisicao.fromProcesso(processo);
+
+            ModelAndView mv = new ModelAndView("processos/edit");
+            mv.addObject("processoId", processo.getNumero());
+            return mv;
+        }
+        return new ModelAndView("redirect:/processos");
+    }
+
+    //Atualiza o registro editado
+    @PostMapping("/{id}")
+    public ModelAndView update(@PathVariable Long id, @Valid DtoProcesso requisicao, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            ModelAndView mv = new ModelAndView("processos/edit");
+            mv.addObject("processoId", id);
+            return mv;
+
+        }
+        else {
+            Optional<Processo> opcional = this.repository.findById(id);
+            if (opcional.isPresent()){
+                Processo processo = requisicao.toProcesso(opcional.get());
+                this.repository.save(processo);
+                return new ModelAndView("redirect:/processos/" + processo.getNumero());
+
+            }
+            return new ModelAndView("redirect:/processos");
+
+
+        }
+    }
+
+    //Excluindo um registro
+    @GetMapping("/{id}/delete")
+    public String delete(@PathVariable Long id){
+        try {
+            this.repository.deleteById(id);
+            return "redirect:/processos";
+        }
+        catch (EmptyResultDataAccessException e){
+            return "redirect:/processos";
+        }
     }
 
 
